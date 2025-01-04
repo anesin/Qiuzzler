b7a87639bb95252b9628f2b536337d5a34815fd8
이재영 <jaeyoung.yi@seculetter.com>
2025-01-04 11:11:41 +0900

    95. What You'll Make by the End of this Module

--- Qiuzzler/ViewController.swift
+++ Qiuzzler/ViewController.swift
@@ -7,13 +7,39 @@
 
 import UIKit
 
-class ViewController: UIViewController {
 
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var trueButton: UIButton!
+    @IBOutlet weak var falseButton: UIButton!
+    @IBOutlet weak var progressBar: UIProgressView!
+    
+    let quiz = [
+        "Four + Two is equal to Six.",
+        "Four - Three is greater than zero.",
+        "Three + Eight is less than Ten."
+    ]
+    var questionIndex = 0
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        updateUI()
     }
 
-
+    
+    @IBAction func answerButtonPressed(_ sender: UIButton) {
+        questionIndex += 1
+        if quiz.count <= questionIndex {
+            questionIndex = 0
+        }
+        updateUI()
+    }
+    
+    
+    func updateUI() {
+        questionLabel.text = quiz[questionIndex]
+    }
+    
 }
-
